87595287689e6faf14233b3aaf0124b46ad008cc
Sachin Prajapati <skprajapati3214@gmail.com>
2025-09-24 15:10:32 +0530

    refactor: Remove deprecated GetApp and Mock sources
    - Remove GetApp source (replaced with TrustRadius)
    - Remove Mock source (cleanup of development code)
    - Update source index to reflect current active sources
    - Streamline codebase for production readiness

--- scripts/js/sources/index.js
+++ scripts/js/sources/index.js
@@ -1,11 +1,9 @@
 import { G2Source } from "./g2.js"
 import { CapterraSource } from "./capterra.js"
-import { GetAppSource } from "./getapp.js"
-import { MockSource } from "./mock.js"
+import { TrustRadiusSource } from "./trustradius.js"
 
 export const SOURCES = {
   g2: G2Source,
   capterra: CapterraSource,
-  getapp: GetAppSource, // bonus
-  mock: MockSource, // for testing/demo purposes
+  trustradius: TrustRadiusSource,
 }
